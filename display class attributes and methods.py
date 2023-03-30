342fa91b8e521362fa728863e815a86769672d82
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-03-30 14:41:45 +0530

    Add files via upload

---
+++ display class attributes and methods.py
@@ -0,0 +1,10 @@
+class demo:
+    name="";
+    age="";
+    def read(self):
+        name=input("enter the name")
+        print("name is..",name)
+        age=input("enter the age")
+        print("age is..",age)
+obj=demo()
+obj.read()
